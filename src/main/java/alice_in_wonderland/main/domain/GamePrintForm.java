0eb306290450e488a38a7d9536f88c68b8daea66
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-13 02:02:15 +0900

    feat : GamePrintForm 출력 form Enum 작성

---
+++ src/main/java/alice_in_wonderland/main/domain/GamePrintForm.java
@@ -0,0 +1,14 @@
+package alice_in_wonderland.main.domain;
+
+public enum GamePrintForm {
+    SIZE_OF_KEY("키의 크기"),
+    SIZE_OF_DOOR("문의 크기"),
+    SIZE_OF_ALICE("앨리스의 크기");
+
+    private final String name;
+
+    GamePrintForm(String name) {
+        this.name = name;
+
+    }
+}
