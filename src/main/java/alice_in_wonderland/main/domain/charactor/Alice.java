cb922d620d4eef4a6ecc452350412e4c270fd460
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-13 00:50:50 +0900

    feat : 앨리스, 키 랜덤생성 인터패이스 변수 생성

--- src/main/java/alice_in_wonderland/main/domain/charactor/Alice.java
+++ src/main/java/alice_in_wonderland/main/domain/charactor/Alice.java
@@ -1,6 +1,16 @@
 package alice_in_wonderland.main.domain.charactor;
 
+import alice_in_wonderland.main.domain.number.NumberMaker;
+
 public class Alice {
+    private final NumberMaker numberMaker;
+
+    private int size = 0;
+
+
+    public Alice(NumberMaker numberMaker) {
+        this.numberMaker = numberMaker;
+    }
 
 
 }
--- src/main/java/alice_in_wonderland/main/domain/charactor/Door.java
+++ src/main/java/alice_in_wonderland/main/domain/charactor/Door.java
@@ -1,13 +1,16 @@
 package alice_in_wonderland.main.domain.charactor;
 
 import alice_in_wonderland.main.domain.gamedata.GoOverState;
+import alice_in_wonderland.main.domain.number.NumberMaker;
 
 public class Door {
 
+    private final NumberMaker numberMaker;
     private final int size;
     private GoOverState goOverState = GoOverState.STAY;
 
-    public Door(int size) {
+    public Door(NumberMaker numberMaker,int size) {
+        this.numberMaker = numberMaker;
         this.size = size;
         //사이즈 생성해야됨
     }
