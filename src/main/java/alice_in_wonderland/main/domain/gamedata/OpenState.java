7f72a4eeb8cd86c65af14b7c61b637fa44b6d356
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-13 00:38:41 +0900

    feat : OpenState Enum 작성

--- src/main/java/alice_in_wonderland/main/domain/gamedata/OpenState.java
+++ src/main/java/alice_in_wonderland/main/domain/gamedata/OpenState.java
@@ -1,4 +1,14 @@
 package alice_in_wonderland.main.domain.gamedata;
 
 public enum OpenState {
+    OPEN("open",true),
+    CLOSED("closed",false);
+
+    private final String name;
+    private final boolean open;
+
+    OpenState(String name, boolean open) {
+        this.name = name;
+        this.open = open;
+    }
 }
