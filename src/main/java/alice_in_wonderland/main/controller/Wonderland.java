2849ed583ca3a115632b1ffa416bcafc128063bf
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-13 02:16:31 +0900

    feat : GameMessage 게임 인트로 멘트 출력 필드명 출력되는 오류 수정 필요

--- src/main/java/alice_in_wonderland/main/controller/Wonderland.java
+++ src/main/java/alice_in_wonderland/main/controller/Wonderland.java
@@ -1,8 +1,12 @@
 package alice_in_wonderland.main.controller;
 
+import alice_in_wonderland.main.util.message.GameMessage;
+import alice_in_wonderland.main.view.Output;
+
 public class Wonderland {
 
     public void main(){
-        System.out.println("Wonderland");
+        GameMessage gameMessage = new GameMessage();
+        Output.printGameIntro(gameMessage);
     }
 }
--- src/main/java/alice_in_wonderland/main/view/Output.java
+++ src/main/java/alice_in_wonderland/main/view/Output.java
@@ -1,4 +1,10 @@
 package alice_in_wonderland.main.view;
 
+import alice_in_wonderland.main.util.message.GameMessage;
+
 public class Output {
+
+    public static void printGameIntro(GameMessage gameMessage){
+        System.out.println(gameMessage);
+    }
 }
