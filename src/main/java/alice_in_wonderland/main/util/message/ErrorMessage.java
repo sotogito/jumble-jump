e1ad6b883728ca61d37db7ee438a16aebf3f6c77
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-14 02:09:14 +0900

    refactor : format+join으로 입력해야될 항목 출력

--- src/main/java/alice_in_wonderland/main/util/message/ErrorMessage.java
+++ src/main/java/alice_in_wonderland/main/util/message/ErrorMessage.java
@@ -1,8 +1,17 @@
 package alice_in_wonderland.main.util.message;
 
+import alice_in_wonderland.main.domain.states.GoOverState;
+import alice_in_wonderland.main.domain.states.Item;
+import alice_in_wonderland.main.domain.states.OpenState;
+
 public class ErrorMessage {
 
-    public final static String INPUT_ERROR ="음료나 버섯이나 open이나 go over 중 하나를 작성해주세요";
+    public final static String INPUT_ERROR =
+            String.format("%s 중 하나를 입력해주세요.",
+                    String.join(", ",
+                            Item.DRINK.getName(),Item.MUSHROOM.getName(),
+                            OpenState.OPEN.getName(),
+                            GoOverState.GO_OVER.getName()));
 
     //앨리스
     public final static String ALICE_SIZE_LIMIT_MAX = "더이상 커질 수 없어ㅜㅜ";
@@ -13,4 +22,5 @@ public class ErrorMessage {
 
     //문
     public final static String CANT_GO_OVER_NOT_YET_OPEN = "키로 문 먼저 열어야해!";
+
 }
