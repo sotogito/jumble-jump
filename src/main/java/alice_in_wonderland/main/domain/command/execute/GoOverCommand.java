a53591498f41c21c36ba42381421549c1d40ae71
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-15 01:15:09 +0900

    refactor : go over입력시 게임 성공 상태 확인하고 throw 로직

--- src/main/java/alice_in_wonderland/main/domain/command/execute/GoOverCommand.java
+++ src/main/java/alice_in_wonderland/main/domain/command/execute/GoOverCommand.java
@@ -3,11 +3,13 @@ package alice_in_wonderland.main.domain.command.execute;
 import alice_in_wonderland.main.domain.charactor.Door;
 import alice_in_wonderland.main.domain.charactor.Key;
 import alice_in_wonderland.main.domain.command.Command;
+import alice_in_wonderland.main.domain.manager.GameManager;
 import alice_in_wonderland.main.util.message.ErrorMessage;
 
 public class GoOverCommand implements Command {
     private final Door door;
     private final Key key;
+   //private final GameManager gameManager;
 
     public GoOverCommand(Door door, Key key) {
         this.door = door;
@@ -18,6 +20,9 @@ public class GoOverCommand implements Command {
     public void execute() {
         if(key.getOpenState()){
             door.changeGoOverState();
+            if(door.getGoOverState() && key.getOpenState()){
+                throw new IllegalArgumentException("성공");
+            }
         }
         throw new IllegalArgumentException(ErrorMessage.CANT_GO_OVER_NOT_YET_OPEN);
     }
