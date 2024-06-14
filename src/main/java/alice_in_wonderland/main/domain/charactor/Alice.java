3a9e17745dc1d6aafa0c279314915bc2474bd1fe
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-15 01:14:17 +0900

    fix : return으로 성공시 예외 던지는거 방지

--- src/main/java/alice_in_wonderland/main/domain/charactor/Alice.java
+++ src/main/java/alice_in_wonderland/main/domain/charactor/Alice.java
@@ -24,6 +24,7 @@ public class Alice {
     public void growSize(){
         if (size < MAX_SIZE){
             size += numberMaker.generate(MIN_SIZE, maxSizeAtDrink());
+            return;
         }
         throw new IllegalArgumentException(ErrorMessage.ALICE_SIZE_LIMIT_MAX);
     }
@@ -31,6 +32,7 @@ public class Alice {
     public void reduceSize(){
         if (size > MIN_SIZE){
             size -= numberMaker.generate(MIN_SIZE, maxSizeAtMushroom());
+            return;
         }
         throw new IllegalArgumentException(ErrorMessage.ALICE_SIZE_LIMIT_MIN);
     }
