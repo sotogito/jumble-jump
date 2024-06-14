9b26d6d10a653c48efac2d442bfe0b9a259cbd41
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-14 23:31:37 +0900

    feat : 버섯command 작성

---
+++ src/main/java/alice_in_wonderland/main/domain/command/execute/MushroomCommand.java
@@ -0,0 +1,17 @@
+package alice_in_wonderland.main.domain.command.execute;
+
+import alice_in_wonderland.main.domain.charactor.Alice;
+import alice_in_wonderland.main.domain.command.Command;
+
+public class MushroomCommand implements Command {
+    private final Alice alice;
+
+    public MushroomCommand(Alice alice) {
+        this.alice = alice;
+    }
+
+    @Override
+    public void execute() {
+        alice.reduceSize();
+    }
+}
