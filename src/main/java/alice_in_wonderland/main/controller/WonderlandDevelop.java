459a176a0396e954dac9f7e435a95faf9df129a5
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-15 01:15:44 +0900

    feat : 컨트롤러 게임 로직 작성

--- src/main/java/alice_in_wonderland/main/controller/WonderlandDevelop.java
+++ src/main/java/alice_in_wonderland/main/controller/WonderlandDevelop.java
@@ -1,8 +1,65 @@
 package alice_in_wonderland.main.controller;
 
+import alice_in_wonderland.main.domain.charactor.Alice;
+import alice_in_wonderland.main.domain.charactor.Door;
+import alice_in_wonderland.main.domain.charactor.Key;
+import alice_in_wonderland.main.domain.command.Command;
+import alice_in_wonderland.main.domain.command.InputManager;
+import alice_in_wonderland.main.domain.manager.GameManager;
+import alice_in_wonderland.main.domain.number.NumberMaker;
+import alice_in_wonderland.main.domain.number.RandomNumberMaker;
+import alice_in_wonderland.main.util.message.ErrorMessage;
+import alice_in_wonderland.main.view.Input;
+import alice_in_wonderland.main.view.Output;
+
 public class WonderlandDevelop {
 
     public void main(){
+        Output.printGameIntro();
+
+        NumberMaker numberMaker = new RandomNumberMaker();
+        Alice alice = new Alice(numberMaker);
+        Door door = new Door(numberMaker);
+        Key key = new Key();
+
+        InputManager inputManager = new InputManager(alice, door, key);
+        GameManager gameManager = new GameManager();
+
+        Output.printSizeAndAttemptCount(gameManager,key,door,alice);
+        boolean isGameEnd = gameLoop(inputManager,gameManager,key,door,alice);
+        if (isGameEnd){
+            Output.printSuccess(gameManager,alice);
+            return;
+        }
+        Output.printFail(alice,door);
 
     }
+
+    private boolean gameLoop(InputManager inputManager, GameManager gameManager,
+                             Key key, Door door, Alice alice){
+
+        while (!gameManager.isOverCount()){
+            try{
+                inputManager.registerCommand(getInput());
+                gameManager.decreaseCount();
+                Output.printSizeAndAttemptCount(gameManager, key, door, alice);
+            }catch (IllegalArgumentException e){
+                if(e.getMessage().contains("성공")) {
+                    return true;
+                }
+                Output.printError(e.getMessage());
+                if (!e.getMessage().contains(ErrorMessage.INPUT_ERROR)){
+                    gameManager.decreaseCount();
+                    Output.printAttemptCount(gameManager);
+                }
+            }
+        }
+        return false;
+    }
+
+    private String getInput(){
+        return Input.inputUser();
+    }
+
+
 }
