4af9494a37624416ad4f417766c3df85e1dcede2
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-19 14:20:07 +0900

    test : mockito 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.20.2")
-
+    testImplementation("org.mockito:mockito-core:4.0.0")
 
 }
 
