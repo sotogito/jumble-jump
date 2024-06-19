d9cdefab17abf5c9061297078aa4082734676ea7
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-19 19:29:05 +0900

    feat : 컨트롤러 Main.java 연결

---
+++ src/main/java/grandmas_letter_aroseagida/Main.java
@@ -0,0 +1,10 @@
+package grandmas_letter_aroseagida;
+
+import grandmas_letter_aroseagida.controller.Aroseagida;
+
+public class Main {
+    public static void main(String[] args) {
+        Aroseagida aroseagida = new Aroseagida();
+        aroseagida.aroseagida();
+    }
+}
---
+++ src/main/java/grandmas_letter_aroseagida/controller/Aroseagida.java
@@ -0,0 +1,7 @@
+package grandmas_letter_aroseagida.controller;
+
+public class Aroseagida {
+
+    public void aroseagida() {
+    }
+}
