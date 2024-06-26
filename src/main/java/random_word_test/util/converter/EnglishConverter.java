d1371a0a73d1cdfd9e64a2bbd176b41a7a3c3ae8
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-27 03:36:21 +0900

    feat : 영어, 뜻 객체 생성에 필요한 converter 인터페이스 적용

---
+++ src/main/java/random_word_test/util/converter/EnglishConverter.java
@@ -0,0 +1,9 @@
+package random_word_test.util.converter;
+
+public class EnglishConverter implements Converter {
+
+    @Override
+    public void convert(String word) {
+        //todo 영어 대문자들 다 소문자로 변환
+    }
+}
---
+++ src/main/java/random_word_test/util/converter/KoreanConverter.java
@@ -0,0 +1,9 @@
+package random_word_test.util.converter;
+
+public class KoreanConverter implements Converter {
+
+    @Override
+    public void convert(String word) {
+        //todo ,를 기준으로 List<string>으로 만듦
+    }
+}
