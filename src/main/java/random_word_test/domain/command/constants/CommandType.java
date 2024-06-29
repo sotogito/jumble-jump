5cfd57bf6fe432890d33f96b8ee95e728ad3ac7a
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-30 01:03:24 +0900

    feat : 입력 command 상수화

--- src/main/java/random_word_test/domain/command/constants/CommandType.java
+++ src/main/java/random_word_test/domain/command/constants/CommandType.java
@@ -1,15 +1,15 @@
 package random_word_test.domain.command.constants;
 
 public enum CommandType {
-    COMPLETED_INPUT_VOCA("입력 완료"),
+    COMPLETED_INPUT_VOCA("입력 완료"), //InputController
 
     TEST_ENGLISH("영어"),
     TEST_KOREAN("뜻"),
-    QUIT_TEST("그만한기"),
+    QUIT_TEST("그만하기"), //test
 
-    REPLAY("다시 시도"),
-    NEW_VACA_TEST("새로운 단어"),
-    END("종료");
+    REPLAY("다시 시도"), //시험만 다시
+    NEW_VACA_TEST("새로운 단어"), //다시 새로 시작하면 됨 //초기화 어떻게하지
+    END("종료"); //TestManager
 
     private final String command;
 
