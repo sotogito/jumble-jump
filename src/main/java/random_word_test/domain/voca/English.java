25b3077b05ca428d5bf853571517ea07c52a49d3
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-29 16:43:09 +0900

    feat : 영어 , 한글 객체 오버라이드

--- src/main/java/random_word_test/domain/voca/English.java
+++ src/main/java/random_word_test/domain/voca/English.java
@@ -1,9 +1,38 @@
 package random_word_test.domain.voca;
 
-public class English {
+public class English implements Comparable<English> {
     private final String word;
 
     public English(String word) {
         this.word = word;
     }
+
+    public String getWord() {
+        return word;
+    }
+
+    @Override
+    public int compareTo(English other) {
+        return this.word.compareTo(other.word); // note TreeMap에 저장될 때 key의 순서를 지정함 = 사전순 대로
+    }
+
+    @Override
+    public boolean equals(Object obj) { //note 영어가 같으면 같은 객체로 취급
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        English english = (English) obj;
+        return word.equals(english.word);
+    }
+
+    @Override
+    public int hashCode() {
+        return word.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return word;
+    }
+
+
 }
--- src/main/java/random_word_test/domain/voca/Korean.java
+++ src/main/java/random_word_test/domain/voca/Korean.java
@@ -3,9 +3,14 @@ package random_word_test.domain.voca;
 import java.util.List;
 
 public class Korean {
-    private final List<String> definition;
+    private final List<String> definitions;
 
-    public Korean(List<String> definition) {
-        this.definition = definition;
+    public Korean(List<String> definitions) {
+        this.definitions = definitions;
+    }
+
+    @Override
+    public String toString() {
+        return String.join(", ", definitions);
     }
 }
