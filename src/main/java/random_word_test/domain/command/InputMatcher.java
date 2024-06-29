f94e171af3278d0c09a4b249f25e7df80ddb9664
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-29 17:36:02 +0900

    feat : 입력왼료할 때까지 입력받기

--- src/main/java/random_word_test/domain/command/InputMatcher.java
+++ src/main/java/random_word_test/domain/command/InputMatcher.java
@@ -1,4 +1,12 @@
 package random_word_test.domain.command;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class InputMatcher {
+
+    private final Map<String, Command> commands = new HashMap<>();
+
+
+
 }
