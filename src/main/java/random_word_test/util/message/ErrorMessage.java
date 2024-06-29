dc59c8a15233290232258bc177d4ada2dd39871e
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-30 01:05:18 +0900

    feat : 에러 메시지 상수화

--- src/main/java/random_word_test/util/message/ErrorMessage.java
+++ src/main/java/random_word_test/util/message/ErrorMessage.java
@@ -1,4 +1,7 @@
 package random_word_test.util.message;
 
 public class ErrorMessage {
+
+    public final static String INVALID_INPUT = "명령어에 없는 입력갑쇼 입니다.";
+    public final static String INVALID_VOCA_ENTRY = "영어 : 뜻 => 의 형태로 입력해주세요.";
 }
