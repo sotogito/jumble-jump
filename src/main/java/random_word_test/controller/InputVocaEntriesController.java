03f6d22c9a809eac3b7bcc21470f5e0930f1c80e
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-30 01:04:56 +0900

    feat : [입력 완료]가 될때까지 입력받기

--- src/main/java/random_word_test/controller/InputVocaEntriesController.java
+++ src/main/java/random_word_test/controller/InputVocaEntriesController.java
@@ -1,6 +1,8 @@
 package random_word_test.controller;
 
+import random_word_test.domain.command.InputVocaEntriesManager;
 import random_word_test.domain.voca.VocaPair;
+import random_word_test.util.validators.CommandFormatValidator;
 import random_word_test.view.Input;
 
 import java.util.ArrayList;
@@ -9,17 +11,25 @@ import java.util.List;
 public class InputVocaEntriesController {
 
     private boolean isInputReceived = true;
+    private final InputVocaEntriesManager inputVocaEntriesManager;
+
+    InputVocaEntriesController() {
+        inputVocaEntriesManager = new InputVocaEntriesManager(this);
+    }
 
     public List<String> getVocaEntries() {
         List<String> vocaEntries = new ArrayList<>();
-
-        while (!isInputReceived){
+        while (isInputReceived){
             String input = Input.inputVocaEntries();
+
+            if(CommandFormatValidator.isContainCommandFormat(input)){
+                inputVocaEntriesManager.inputMatch(input);
+                break;
+            }
             vocaEntries.add(input);
         }
         return vocaEntries;
     }
-
 
     public void setInputReceivedFalse(){
         this.isInputReceived = false;
---
+++ src/main/java/random_word_test/domain/command/InputVocaEntriesManager.java
@@ -0,0 +1,37 @@
+package random_word_test.domain.command;
+
+import random_word_test.controller.InputVocaEntriesController;
+import random_word_test.domain.command.constants.CommandType;
+import random_word_test.domain.command.execute.InputCompletedCommand;
+import random_word_test.util.message.ErrorMessage;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InputVocaEntriesManager {
+    //TODO 입력완료
+
+    private final Map<String, Command> commandMap = new HashMap<>();
+
+    public InputVocaEntriesManager(InputVocaEntriesController inputController) {
+        initCommandMap(inputController);
+    }
+
+    public void inputMatch(String input){
+        Command command = commandMap.get(input);
+        if(command != null){
+            command.execute();
+            return;
+        }
+
+        throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT);
+    }
+
+    private void initCommandMap(InputVocaEntriesController inputController){
+        commandMap.put(CommandType.COMPLETED_INPUT_VOCA.getCommand(), new InputCompletedCommand(inputController));
+    }
+
+
+
+}
