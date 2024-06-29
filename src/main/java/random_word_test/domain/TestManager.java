a30dc751c7ee1448dba52f763d16c2d25d7f0148
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-29 17:43:20 +0900

    feat : 게임 전체 제어하는 TestManager 생성

---
+++ src/main/java/random_word_test/domain/TestManager.java
@@ -0,0 +1,15 @@
+package random_word_test.domain;
+
+public class TestManager {
+
+    private boolean isTestEnded = false;
+
+
+    public boolean isTestEnded() {
+        return isTestEnded;
+    }
+
+    public void setTestEnded() {
+        this.isTestEnded = true;
+    }
+}
