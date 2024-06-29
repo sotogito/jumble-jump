f3c188c4244bf5722b172a8e5ed65facf29d9761
sotogito <108535526+sotogito@users.noreply.github.com>
2024-06-29 17:34:41 +0900

    feat : 입력왼료할 때까지 입력받기

--- src/main/java/random_word_test/domain/command/execute/InputCompletedCommand.java
+++ src/main/java/random_word_test/domain/command/execute/InputCompletedCommand.java
@@ -1,10 +1,18 @@
 package random_word_test.domain.command.execute;
 
+import random_word_test.controller.InputVocaEntriesController;
 import random_word_test.domain.command.Command;
 
 public class InputCompletedCommand implements Command {
+
+    private final InputVocaEntriesController inputController;
+
+    public InputCompletedCommand(InputVocaEntriesController inputController) {
+        this.inputController = inputController;
+    }
+
     @Override
     public void execute(){
-
+        inputController.setInputReceivedFalse();
     }
 }
