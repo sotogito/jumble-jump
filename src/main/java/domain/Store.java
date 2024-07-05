5a5de764f8f8d02d11091c56a4778406fa36d628
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-06 02:41:06 +0900

    feat : 계속 구매할 수 있는지 판단 로직 생성

---
+++ src/main/java/domain/Store.java
@@ -0,0 +1,48 @@
+package domain;
+
+import domain.item.Item;
+import domain.item.Items;
+
+public class Store {
+    private Order order;
+    private final Items items;
+    private final UserCashier userCashier;
+    private Item minimumPrice;
+
+    //돈은 부족하지 않은데 수량이 부족하면? 돈은 업데이트 된거잖아
+    //수량은있는데 돈이 부족하면? 수량은 업데이트 된거잖아
+    //각각 유효처리 한거를 따로 분리해야될듯
+
+    public Store(Items items, UserCashier userCashier) {
+        this.userCashier = userCashier;
+        this.order = new Order();
+        this.items = items;
+    }
+
+    public boolean isCanPurchase(String itemName, int quantity){
+        Item item = items.findItemByName(itemName);
+
+        //note 최소 금액보다 일단 잔돈이 많은지
+        if(userCashier.isLessThanPurchasedItemAmount(minimumPrice.getPrice())){
+            return false;
+        }
+
+        long purchasedPrice = item.calculatePurchasePriceAsAmount(quantity);
+
+        //note 일단 구매 하면 구매할 돈이 있고, 충분한 수량이 있는지 확인
+        item.validateAfterPurchasingStock(quantity);
+        userCashier.validateSufficientAmount(purchasedPrice);
+
+
+        //note 성공하면 업데이트
+        userCashier.decreaseAmountAsPurchased(purchasedPrice);
+        item.decreaseStock(quantity);
+
+
+        if(item.equals(minimumPrice) && minimumPrice.isOutOfStock()){ //구매했는데 만약 상품이 0개라면 최
+            minimumPrice = items.getMinimumPriceItem();
+        }
+
+        return true;
+    }
+}
