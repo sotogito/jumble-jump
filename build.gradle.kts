0f19c1788e4e73a4f51c37790ad6a10094579d98
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-06 03:46:13 +0900

    feat : mock 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation ("org.assertj:assertj-core:3.21.0")
+    testImplementation ("org.mockito:mockito-core:4.2.0")
+    testImplementation ("org.mockito:mockito-junit-jupiter:4.2.0")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.test {
