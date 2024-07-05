0184f38961726a1cc4d4162dce7d7c16bfb2a873
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-05 16:35:09 +0900

    feat : Application.java에 메인 컨트롤러 연결

--- docs/Note.md
+++ docs/Note.md
@@ -1,6 +1,6 @@
 ### 구현 순서
-1. CSV 파일 생성 및 읽어오기
 2. item
+1. CSV 파일 생성 및 읽어오기
 3. UserCashier
 4. Order
 5. Store
---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class MainController {
+    public void main(){
+        System.out.println("Hello World");
+    }
+}
