a8925a5887fcecd939e5fc096be5005fade24a56
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-06 14:03:02 +0900

    feat : 잔돈 계산기 작성

---
+++ src/main/java/domain/ChangeCalculator.java
@@ -0,0 +1,19 @@
+package domain;
+
+import java.util.EnumMap;
+
+public class ChangeCalculator {
+
+    public EnumMap<Bill, Integer> calculateChange(int balance) {
+        EnumMap<Bill, Integer> result = new EnumMap<>(Bill.class);
+
+        for (Bill coin : Bill.values()) {
+            int coinValue = coin.getValue();
+            int count = balance / coinValue;  // 각 동전의 개수를 계산
+            balance %= coinValue;             // 잔액을 업데이트
+            result.put(coin, count);
+        }
+        return result;
+    }
+
+}
