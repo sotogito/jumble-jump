ccca218c37b0518b025f4ce3f2b966c211eb8f60
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-06 15:37:36 +0900

    feat : 아이템 목록 출력하기

---
+++ src/main/java/view/printer/ItemListPrinter.java
@@ -0,0 +1,30 @@
+package view.printer;
+
+import domain.item.Item;
+import domain.item.Items;
+
+public class ItemListPrinter {
+    private static final String PRINT_ITEM_LIST_INTRO = "<구매 가능 상품 목록>\n";
+    private static final String PRINT_ITEM_LIST_FORMAT = "- %s : %,d원\n";
+    private final Items items;
+
+    public ItemListPrinter(Items items) {
+        this.items = items;
+    }
+
+    public void print(){
+        System.out.print(PRINT_ITEM_LIST_INTRO);
+        System.out.print(getItemList());
+
+    }
+
+    private String getItemList(){
+        StringBuilder sb = new StringBuilder();
+        for(Item item : items.getItems()){
+            String printout = String.format(PRINT_ITEM_LIST_FORMAT, item.getName(), item.getPrice());
+            sb.append(printout);
+        }
+        return sb.toString();
+    }
+
+}
