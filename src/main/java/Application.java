9bffa397633cf3ba490d7126a00cafc1cdce74cc
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-06 15:00:35 +0900

    feat : 아이템 csv 파일 읽어오는 코드 Application.java로 이동

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,25 @@
 import controller.MainController;
+import domain.item.Item;
+import domain.item.Items;
+import util.reader.ItemCsvReader;
+import util.reader.RawCsvReader;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) throws IOException {
         MainController mainController = new MainController();
+        registerVendingMachineItems();
         mainController.main();
     }
+
+    public static void registerVendingMachineItems() throws IOException {
+        ItemCsvReader csvReader = new ItemCsvReader(new RawCsvReader());
+        String itemCsvPath = "/data/items.csv";
+
+        List<Item> itemList = csvReader.readItems(itemCsvPath);
+        Items items = new Items(itemList);
+    }
+
 }
--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -11,13 +11,7 @@ import java.util.List;
 
 public class MainController {
     public void main() throws IOException {
-        ItemCsvReader csvReader = new ItemCsvReader(new RawCsvReader());
-        String itemCsvPath = "/data/items.csv";
 
-        List<Item> itemList = csvReader.readItems(itemCsvPath);
-        Items items = new Items(itemList);
-
-        System.out.println(items.findItemByName("씹던 껌").getName());
 
     }
 }
