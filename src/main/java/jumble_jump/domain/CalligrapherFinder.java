2ef1971752ed021ce31ae9f14b78ba6dfa61c4f8
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-09 23:54:32 +0900

    feat : 폰트 아이디, 출력물,[그만하기] 입력받고 분류

---
+++ src/main/java/jumble_jump/domain/CalligrapherFinder.java
@@ -0,0 +1,34 @@
+package jumble_jump.domain;
+
+import jumble_jump.domain.calligrapher.BasicsCalligrapher;
+import jumble_jump.domain.calligrapher.Calligrapher;
+import jumble_jump.domain.calligrapher.VariousCalligrapher;
+
+public class CalligrapherFinder {
+    private final Fonts fonts;
+    private final String endStatus = Status.END.getStatus();
+    //private boolean isEnd;
+
+    public CalligrapherFinder(Fonts fonts) {
+        this.fonts = fonts;
+    }
+
+    public boolean isEnd(String input){
+        return input.equals(endStatus);
+    }
+
+    public String getCalligrapy(int fontId, String printout){
+        String fontName = fonts.getFontName(fontId);
+        Calligrapher calligrapher = getCalligrapher(fontId);
+
+        return calligrapher.write(fontName, printout);
+    }
+
+    private Calligrapher getCalligrapher(int fontId){
+        if (fontId == 0){
+            return new BasicsCalligrapher();
+        }
+        return new VariousCalligrapher();
+    }
+
+}
