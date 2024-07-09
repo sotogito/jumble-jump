644f3338362b2048fbe606763130a231c212bab0
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-09 23:55:25 +0900

    feat : output 작성

---
+++ src/main/java/jumble_jump/view/Output.java
@@ -0,0 +1,11 @@
+package jumble_jump.view;
+
+public class Output {
+    public static void printError(String error) {
+        System.out.print("[ERROR] " + error);
+    }
+
+    public static void printMessage(String message){
+        System.out.print(message);
+    }
+}
