a6b4bf66a3fdd8a2ec0f5620c1eb3f519e2a9eec
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-09 23:55:10 +0900

    feat : 출력물 , 아이디 입력 input 작성

---
+++ src/main/java/jumble_jump/view/Input.java
@@ -0,0 +1,23 @@
+package jumble_jump.view;
+
+import java.util.Scanner;
+
+public class Input {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String inputPrintout(){
+        System.out.println("출력물을 입력해주세요.\n");
+        return scanner.nextLine();
+    }
+
+    public static int inputFontId(){
+        System.out.println("원하는 폰트 ID 를 입력해 주세요.\n");
+        try{
+            return Integer.parseInt(scanner.nextLine())-1;
+        }catch (IllegalArgumentException e){
+            throw  new IllegalArgumentException("숫자로 입력하세요\n");
+        }
+    }
+
+
+}
