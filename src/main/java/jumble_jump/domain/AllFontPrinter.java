5d8ae5c4f3d5a500af4dd57bc62b292de54bba25
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-09 23:57:14 +0900

    feat : 모든 폰트 출력하는 printer작성

---
+++ src/main/java/jumble_jump/domain/AllFontPrinter.java
@@ -0,0 +1,23 @@
+package jumble_jump.domain;
+
+import jumble_jump.domain.calligrapher.Calligrapher;
+
+import java.awt.*;
+import java.security.KeyStore;
+import java.util.Map;
+
+public class AllFontPrinter {
+
+    public String print(CalligrapherFinder finder, Fonts fonts){
+        StringBuilder result = new StringBuilder();
+
+        for(Map.Entry<Integer, String> entry : fonts.getFonts().entrySet()){
+            String printout = finder.getCalligrapy(entry.getKey(),entry.getValue());
+            result.append(entry.getKey()+1).append(":")
+                    .append("\n")
+                    .append(printout)
+                    .append("\n");
+        }
+        return result.toString();
+    }
+}
