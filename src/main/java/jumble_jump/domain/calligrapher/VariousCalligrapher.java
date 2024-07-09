84f452fc0f89c226c04829070b59caabc3c2cddc
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-09 22:51:08 +0900

    feat : 경로 파일에서 글씨체 출력

---
+++ src/main/java/jumble_jump/domain/calligrapher/VariousCalligrapher.java
@@ -0,0 +1,23 @@
+package jumble_jump.domain.calligrapher;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class VariousCalligrapher implements Calligrapher {
+
+    private static final String FONT_PATH = "/fonts/ours/%s%s";
+    private static final String EXTENSION_PATH = ".flf";
+
+    @Override
+    public String write(String fontName, String printout) {
+        try{
+            String path = String.format(FONT_PATH, fontName, EXTENSION_PATH);
+            InputStream fontStream = VariousCalligrapher.class.getResourceAsStream(path);
+            return FigletFont.convertOneLine(fontStream, printout);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
