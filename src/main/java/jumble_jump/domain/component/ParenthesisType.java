e50c321ca64b1f56c1f7a9085b5cd841c9b0b2f4
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-19 01:11:59 +0900

    feat : 괄호 우선순위 필드 추가 및 우선순위 비교 메서드 작성

---
+++ src/main/java/jumble_jump/domain/component/ParenthesisType.java
@@ -0,0 +1,46 @@
+package jumble_jump.domain.component;
+
+public enum ParenthesisType {
+    PARENTHESIS_LEFT('(', 1),
+    PARENTHESIS_RIGHT(')', 1),
+    CURLY_BRACE_LEFT('{', 2),
+    CURLY_BRACE_RIGHT('}', 2),
+    BRACKET_LEFT('[', 3),
+    BRACKET_RIGHT(']', 3),
+    ;
+
+    private final char symbol;
+    private final int priority;
+
+    ParenthesisType(char symbol, int priority) {
+        this.symbol = symbol;
+        this.priority = priority;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public static ParenthesisType fromSymbol(char symbol) {
+        for (ParenthesisType p : values()) {
+            if (p.getSymbol() == symbol) {
+                return p;
+            }
+        }
+        throw new IllegalArgumentException("존재하지 않는 괄호입니다.");
+    }
+
+    public ParenthesisType getAdvancedPriority(ParenthesisType p1, ParenthesisType p2){
+        if (p1.getPriority() > p2.getPriority()) {
+            return p1;
+        }
+        return p2;
+    }
+
+
+
+}
