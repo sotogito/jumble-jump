689a54684e44d56ed246c97080aaaaf7c94adf3e
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-19 02:12:56 +0900

    feat : 문제 입력받고 List<Token> 반환

---
+++ src/main/java/jumble_jump/util/Tokenizer.java
@@ -0,0 +1,61 @@
+package jumble_jump.util;
+
+import jumble_jump.domain.component.OperatorType;
+import jumble_jump.domain.component.ParenthesisType;
+import jumble_jump.domain.matcher.Matcher;
+import jumble_jump.domain.matcher.NumberMatcher;
+import jumble_jump.domain.matcher.OperatorMatcher;
+import jumble_jump.domain.matcher.ParenthesisMatcher;
+import jumble_jump.domain.token.NumberToken;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tokenizer {
+    private final NumberMatcher numberMatcher;
+    private final OperatorMatcher operatorMatcher;
+    private final ParenthesisMatcher parenthesisMatcher;
+
+    public Tokenizer(
+            NumberMatcher numberMatcher, OperatorMatcher operatorMatcher, ParenthesisMatcher parenthesisMatcher) {
+        this.numberMatcher = numberMatcher;
+        this.operatorMatcher = operatorMatcher;
+        this.parenthesisMatcher = parenthesisMatcher;
+    }
+
+    public List<Token> tokenize(String inputProblem) {
+        List<Token> result = new ArrayList<>();
+        char[] chars = inputProblem.toCharArray();
+
+        for (char c : chars) {
+            if(Character.isDigit(c)){
+                result.add(getNumberToken(c));
+                continue;
+            } else if (OperatorType.isOperatorType(c)) {
+                result.add(getOperatorToken(c));
+                continue;
+            } else if (ParenthesisType.isParenthesisType(c)) {
+                result.add(getParathesisToken(c));
+                continue;
+            }
+            throw new IllegalArgumentException("잘못된 양식의 식입니다.");
+        }
+        return result;
+    }
+
+    private Token getNumberToken(char c){
+        return numberMatcher.match(c);
+    }
+
+    private Token getOperatorToken(char c){
+        return operatorMatcher.match(c);
+    }
+
+    private Token getParathesisToken(char c){
+        return parenthesisMatcher.match(c);
+    }
+
+
+
+
+}
