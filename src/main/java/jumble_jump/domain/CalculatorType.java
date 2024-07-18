6c3c86dd36871cc0f4f44172259e031408eca6e7
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-18 19:08:36 +0900

    feat :계산 종류 인터페이스 생성

---
+++ src/main/java/jumble_jump/domain/CalculatorType.java
@@ -0,0 +1,8 @@
+package jumble_jump.domain;
+
+public interface CalculatorType {
+    double add(double x, double y);
+    double sub(double x, double y);
+    double div(double x, double y);
+    double mul(double x, double y);
+}
