c4a95e5bbdb6fb750d9be79b0135bdfcc56cdf4d
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-19 03:01:41 +0900

    feat : ㅣList<Token>문제 String으로 변환 기능 util화

--- src/main/java/jumble_jump/domain/Problem.java
+++ src/main/java/jumble_jump/domain/Problem.java
@@ -5,6 +5,7 @@ import jumble_jump.domain.token.NumberToken;
 import jumble_jump.domain.token.OperatorToken;
 import jumble_jump.domain.token.ParenthesisToken;
 import jumble_jump.util.Token;
+import jumble_jump.util.converter.ProblemToStringConverter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,19 +22,6 @@ public class Problem {
     }
 
     public static String getProblemText() {
-        List<String> result = new ArrayList<>();
-        for (Token token : problemTokens) {
-            if(token instanceof NumberToken) {
-                int number = ((NumberToken) token).getNumber();
-                result.add(String.valueOf(number));
-            }else if(token instanceof OperatorToken) {
-                OperatorType operatorType = ((OperatorToken) token).getOperatorType();
-                result.add(Character.toString(operatorType.getSymbol()));
-            }else if(token instanceof ParenthesisToken) {
-                result.add(((ParenthesisToken) token).getParenthesisText());
-            }
-        }
-
-        return String.join(" ", result);
+        return ProblemToStringConverter.getProblemText(problemTokens);
     }
 }
