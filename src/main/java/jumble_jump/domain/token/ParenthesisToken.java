9ceffa74607acf2b51de9e1e45f3436e5d047925
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-19 01:25:47 +0900

    feat : 괄호 인터페이스 작성 및 Token 인터페이스 상속으로 묶기

---
+++ src/main/java/jumble_jump/domain/token/ParenthesisToken.java
@@ -0,0 +1,7 @@
+package jumble_jump.domain.token;
+
+import jumble_jump.util.Token;
+
+public interface ParenthesisToken extends Token {
+     int getParenthesisPriority();
+}
---
+++ src/main/java/jumble_jump/domain/token/parenthesis/Parenthesis.java
@@ -0,0 +1,24 @@
+package jumble_jump.domain.token.parenthesis;
+
+import jumble_jump.domain.component.ParenthesisType;
+import jumble_jump.domain.token.ParenthesisToken;
+import jumble_jump.util.Token;
+
+/**
+ * 객체들의 인터페이스 implments는 부모가 아니라 자식으로 해줘야함
+ * 그래야 나중에 List<Token>에서 instanceof으로 숫자인지, 연잔자인지, 괄호인지 알 수 있고
+ * 자식 인터페이스의 메서드를 사용하여 계산할 수 있음
+ */
+public class Parenthesis implements ParenthesisToken {
+
+    private final ParenthesisType parenthesisType;
+
+    public Parenthesis(ParenthesisType parenthesisType) {
+        this.parenthesisType = parenthesisType;
+    }
+
+    @Override
+    public int getParenthesisPriority() {
+        return parenthesisType.getPriority();
+    }
+}
