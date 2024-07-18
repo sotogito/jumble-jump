c38633d487447cbd4a3f4589b87974d065ba6882
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-18 18:53:29 +0900

    feat : 연산자 상수 Enum으로 선언

---
+++ src/main/java/jumble_jump/domain/OperatorType.java
@@ -0,0 +1,27 @@
+package jumble_jump.domain;
+
+public enum OperatorType {
+    ADD('+'),
+    SUBTRACT('-'),
+    MULTIPLY('*'),
+    DIVIDE('/');
+
+    private final char symbol;
+
+    OperatorType(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public static OperatorType fromSymbol(char symbol) {
+        for (OperatorType op : values()) {
+            if (op.getSymbol() == symbol) {
+                return op;
+            }
+        }
+        throw new IllegalArgumentException("사칙연사에 해당하지 않는 연산자가 있습니다.");
+    }
+}
