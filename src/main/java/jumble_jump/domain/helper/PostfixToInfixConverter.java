487843ee34421ad14c170717e9d3c19349752714
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-25 01:05:53 +0900

    fix : 문제풀이 자료형 Stack으로 변경

--- src/main/java/jumble_jump/domain/helper/PostfixToInfixConverter.java
+++ src/main/java/jumble_jump/domain/helper/PostfixToInfixConverter.java
@@ -1,48 +1,46 @@
 package jumble_jump.domain.helper;
 
+import jumble_jump.domain.token.NumberToken;
+import jumble_jump.domain.token.OperatorToken;
+import jumble_jump.domain.token.ParenthesisToken;
+import jumble_jump.util.Token;
+import org.springframework.expression.spel.ast.Operator;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.Stack;
+
 public class PostfixToInfixConverter {
-/*
-    public static void main(String[] args) {
-        String postfix = "3 4 2 2 1 - + * 2 + +";
-        System.out.println("중위 표기법: " + convert(postfix));
-    }
 
-    static String convert(String postfix) {
-        Stack<String> stack = new Stack<>();
-        String[] tokens = postfix.split(" ");
+    private static List<Token> intermediateStepPostfix;
 
-        for (String token : tokens) {
-            if (isOperator(token)) {
-                String b = stack.pop();
-                String a = stack.pop();
-                String expr = "(" + a + token + b + ")";
-                stack.push(expr);
-            } else {
-                stack.push(token);
+    public static String getIntermediateStep(Stack<Token> operatorStack, List<Token> postfix, int startIndex){
+        makeIntermediateStepPostfix(operatorStack, postfix, startIndex);
+        StringBuilder result = new StringBuilder();
+
+
+        for (Token token : intermediateStepPostfix) {
+            if(token instanceof NumberToken){
+                result.append(((NumberToken) token).getNumber());
+            }else if(token instanceof OperatorToken){
+                result.append(((OperatorToken) token).getOperatorType().getSymbol());
+            } else if (token instanceof ParenthesisToken) {
+                result.append(((ParenthesisToken)token).getParenthesisType().getSymbol());
+                //짝이 하나면 .......?
+                //
             }
+        }//{(1+2+3*4)-5}
+
+
+
+        return result.toString();
+    }
+
+    private static void makeIntermediateStepPostfix(Stack<Token> operatorStack, List<Token> postfix, int startIndex){
+        intermediateStepPostfix = new ArrayList<>(operatorStack);
+        for(int i=startIndex; i<postfix.size(); i++){
+            intermediateStepPostfix.add(postfix.get(i));
         }
-        return stack.pop();
     }
-
-    static boolean isOperator(String token) {
-        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
-    }
-
- */
-
-    /**
-     * 만약에 {2*(3+4*5)}를 후위표기법으로 변환하면
-     * 2,3,4,5,),*,+,(,},*,{
-     * 이다.
-     * 합차기 위해서는
-     * 숫자를 스택에 넣고
-     * 열린괄호가 나타나면 닫힌 괄호가 나타날때까지 스택에서 뽑아서 연산자와 계산을 한다.
-     *
-     * )
-     * 4*5
-     * 3+4*5
-     * (
-     * open괄호와 닫힌 괄호 사이에 합친 식을 넣어 최종 스태겡 저장한다.
-     *
-     */
 }
--- src/main/java/jumble_jump/service/CalculatorService.java
+++ src/main/java/jumble_jump/service/CalculatorService.java
@@ -2,16 +2,15 @@ package jumble_jump.service;
 
 import jumble_jump.domain.Problem;
 import jumble_jump.domain.Solving;
-import jumble_jump.domain.matcher.NumberMatcher;
+import jumble_jump.domain.helper.InfixPostFixHelper;
+import jumble_jump.domain.helper.PostfixToInfixConverter;
 import jumble_jump.domain.token.NumberToken;
 import jumble_jump.domain.token.OperatorToken;
 import jumble_jump.domain.token.ParenthesisToken;
 import jumble_jump.domain.token.number.Number;
-import jumble_jump.domain.type.ParenthesisType;
 import jumble_jump.util.DecimalPointFormatter;
 import jumble_jump.util.Token;
 
-import java.lang.reflect.ParameterizedType;
 import java.util.*;
 
 /**
@@ -25,6 +24,7 @@ public class CalculatorService {
     private final Problem problem;
     private final InfixPostFixHelper infixPostFixHelper;
 
+    private Stack<Token> resultStack = new Stack<>();
     private Double result;
 
 
@@ -36,49 +36,89 @@ public class CalculatorService {
 
     public void calculate(){
         List<Token> postfix = convertToPostfix();
-        //Deque<Double> operatorStack = new ArrayDeque<>();//얘도 토큰
-        Deque<Token> operatorStack = new ArrayDeque<>();//얘도 토큰
+
+        StringBuilder sb = new StringBuilder();
+        for(Token token : postfix) {
+            if(token instanceof NumberToken) {
+                sb.append(((NumberToken) token).getNumber());
+            }else if(token instanceof ParenthesisToken) {
+                sb.append(((ParenthesisToken)token).getParenthesisType().getSymbol());
+            }else if(token instanceof OperatorToken) {
+                sb.append(((OperatorToken)token).getOperatorType().getSymbol());
+            }
+        }
+        System.out.println("-----------");
+        System.out.println(sb);
+        System.out.println("-----------");
+
 
         for (int i = 0; i < postfix.size(); i++) {
             Token token = postfix.get(i);
             if (token instanceof NumberToken) {
-                System.out.print(((NumberToken) token).getNumber() + " ");
-                operatorStack.push(token);
+                //System.out.print(((NumberToken) token).getNumber() + " ");
+                resultStack.push(token);
                 //todo interMediateStep에 추가
             }else if (token instanceof OperatorToken) {
-                System.out.print(((OperatorToken) token).getOperatorType() + " ");
-                double num2 = (((NumberToken) operatorStack.pop()).getNumber());
-                double num1 = (((NumberToken) operatorStack.pop()).getNumber());
+                //System.out.print(((OperatorToken) token).getOperatorType() + " ");
+                double num2 = (((NumberToken) resultStack.pop()).getNumber());
+                double num1 = (((NumberToken) resultStack.pop()).getNumber());
                 double result = ((OperatorToken) token).calculate(num1, num2);
 
-                operatorStack.push(new Number(result));
+                resultStack.push(new Number(result));
 
 
-                convert(operatorStack,postfix,i);
-
+                System.out.println("\n"+PostfixToInfixConverter.getIntermediateStep(resultStack,postfix,i+1)+"\n");
+            } else if (token instanceof ParenthesisToken) {
+                System.out.println("괄호"+((ParenthesisToken)token).getParenthesisType().getSymbol());
+                continue;
             }
-            //todo 중간 출력
+            //todo 괄호면 skip
             solving.updateNumberOfSolving();
         }
 
-        result = (((NumberToken)operatorStack.pop()).getNumber());
+        result = (((NumberToken)resultStack.pop()).getNumber());
         System.out.println(getFormattedResult()+"정");
     }
 
-    public void convert(Deque<Token> operatorStack, List<Token> postfix,int startIndex){
+    public void convert(Deque<Token> operatorStack, List<Token> postfix, int startIndex) {
+        //StringBuilder
         /**
-         * 숫자만 추가하다가
-         * 계산되면 계산식 추기
+         * 닫힌괄호 들어오면 열린괄호 만날때까지 식 num1,num2로 식 완성시키고 StringBuillder에 넣기
+         * 괄호 섹션이 완성되면 넣기 앞에? SB를 스택에 넣음
+         * 넣는 자료형은 Deque<String>이여야함 - 계산이 아니라 꺼내서 합치면 되기 때문에 상관 없음
+         *
+         * 만약에 같은 종류의 괄호가 나오면 괄호의 종류만 판단하고 처음 들어오는 괄호는 무조건 소 -> 중-> 대로 한다.
+         *
+         *
+         *
          */
-
+        List<String> intermediateSteps = new ArrayList<>();
         List<Token> result = new ArrayList<>(operatorStack);
-
-        for(int i=startIndex+1; i<postfix.size(); i++){
+        for (int i = startIndex + 1; i < postfix.size(); i++) {
             result.add(postfix.get(i));
         }
-        System.out.println(result+"\n");
 
+        Deque<String> stack = new ArrayDeque<>();
+        for (Token token : result) {
+            if (token instanceof NumberToken) {
+                stack.push(String.valueOf(((NumberToken) token).getNumber()));
+            } else if (token instanceof OperatorToken) {
+                String num2 = stack.pop();
+                String num1 = stack.pop();
+                String intermediateResult = "(" + num1 + " " + ((OperatorToken) token).getOperatorType().getSymbol() + " " + num2 + ")";
+                stack.push(intermediateResult);
+            }
+        }
+        intermediateSteps.add(stack.peek());
+        printIntermediateSteps(intermediateSteps);
     }
+
+    private void printIntermediateSteps(List<String> intermediateSteps) {
+        for (String step : intermediateSteps) {
+            System.out.println("Intermediate step: [" + step+"]");
+        }
+    }
+
 
     public java.lang.Number getFormattedResult(){
         return DecimalPointFormatter.format(result);
