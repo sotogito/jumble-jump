5ab537810d5b5931f00abd32ef26fd869313b639
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-26 03:30:15 +0900

    fix : 연산자 뒤에 - 음수처리

--- src/main/java/jumble_jump/domain/converter/PostfixToInfixConverter.java
+++ src/main/java/jumble_jump/domain/converter/PostfixToInfixConverter.java
@@ -18,36 +18,14 @@ public class PostfixToInfixConverter {
 
     public static String getIntermediateStep(Stack<Token> operatorStack, List<Token> postfix, int startIndex){
         makeIntermediateStepPostfix(operatorStack, postfix, startIndex);
-        StringBuilder result = new StringBuilder();
-
-
-        for (Token token : intermediateStepPostfix) {
-            if(token instanceof NumberToken){
-                Number numberFormat = DecimalPointFormatter.format(((NumberToken) token).getNumber());
-                result.append(numberFormat);
-            }else if(token instanceof OperatorToken){
-                result.append(((OperatorToken) token).getOperatorType().getSymbol());
-            } else if (token instanceof ParenthesisToken) {
-                result.append(((ParenthesisToken)token).getParenthesisType().getSymbol());
-                //짝이 하나면 .......?
-                //
-            }
-        }//{(1+2+3*4)-5}
-
 
         for(int i  = 0; i < intermediateStepPostfix.size(); i++){
             Token token = intermediateStepPostfix.get(i);
 
             if(token instanceof NumberToken){
-                Number numberFormat = DecimalPointFormatter.format(((NumberToken) token).getNumber());
-                resultStack.push(String.valueOf(numberFormat));
+                updateNumber(token);
             } else if (token instanceof OperatorToken) {
-                String num2 = resultStack.pop();
-                String num1 = resultStack.pop();
-                char operator = ((OperatorToken) token).getOperatorType().getSymbol();
-                resultStack.push(
-                        String.format("%s%s%s", num1, operator, num2)
-                );
+                updateOperator(token);
             } else if (token instanceof ParenthesisToken) {
                 ParenthesisToken parenthesisToken = (ParenthesisToken) token;
 
@@ -63,7 +41,7 @@ public class PostfixToInfixConverter {
                         count++;
                     }
 
-                    if(count > 0){
+                    if(count != 0){
                         String problemPart = resultStack.pop();
                         char openParenthesis = parenthesisToken.getParenthesisType().getSymbol();
                         char closeParenthesis = ParenthesisType.findCloseTypeByOpen(parenthesisToken).getSymbol();
@@ -77,10 +55,66 @@ public class PostfixToInfixConverter {
         return resultStack.pop();
     }
 
+    private static void updateParenthesis(Token token, int i){
+        ParenthesisToken parenthesisToken = (ParenthesisToken) token;
+
+        if(parenthesisToken.isOpenParenthesis()){
+            //열린괄호 바로 앞에 operator이 아닐떄까지 int ++
+
+            int count = 0;
+            for(int j = i-1; j > 0 ; j--){
+                Token token1 = intermediateStepPostfix.get(j);
+                if(!(intermediateStepPostfix.get(j) instanceof OperatorToken)){//열린괄호의 바오 앞에 있는 연산자만
+                    break;
+                }
+                count++;
+            }
+
+            if(count > 0){
+                String problemPart = resultStack.pop();
+                char openParenthesis = parenthesisToken.getParenthesisType().getSymbol();
+                char closeParenthesis = ParenthesisType.findCloseTypeByOpen(parenthesisToken).getSymbol();
+                resultStack.push(
+                        String.format("%s%s%s", openParenthesis, problemPart, closeParenthesis)
+                );
+            }
+        }
+    }
+
+    //private static void
+
+    private static void updateNumber(Token token){
+        Number numberFormat = DecimalPointFormatter.format(((NumberToken) token).getNumber());
+//
+//        double number = ((NumberToken) token).getNumber();
+//        if(number < 0){
+//            resultStack.push("("+String.valueOf(numberFormat)+")");
+//            return;
+//        }
+
+
+        resultStack.push(String.valueOf(numberFormat));
+    }
+
+    private static void updateOperator(Token token){
+        String num2 = resultStack.pop();
+        String num1 = resultStack.pop();
+        char operator = ((OperatorToken) token).getOperatorType().getSymbol();
+        resultStack.push(
+                String.format("%s%s%s", num1, operator, num2)
+        );
+    }
+
+
+
+
+
+
     private static void makeIntermediateStepPostfix(Stack<Token> operatorStack, List<Token> postfix, int startIndex){
         intermediateStepPostfix = new ArrayList<>(operatorStack);
         for(int i=startIndex; i<postfix.size(); i++){
             intermediateStepPostfix.add(postfix.get(i));
         }
     }
+
 }
