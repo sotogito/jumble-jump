193c0951326418588bc0078008b70aa5d320b0ed
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-26 01:08:39 +0900

    refactor : 토큰 출력 코드 삭제

--- src/main/java/jumble_jump/service/CalculatorService.java
+++ src/main/java/jumble_jump/service/CalculatorService.java
@@ -35,19 +35,6 @@ public class CalculatorService {
         this.solving = solving;
         this.infixPostFixHelper = infixPostFixHelper;
         this.postfix = infixPostFixHelper.convertToPostFix(problem);
-
-        StringBuilder sb = new StringBuilder();
-        for (Token token : postfix) {
-            if(token instanceof NumberToken){
-                sb.append(((NumberToken) token).getNumber());
-            }else if(token instanceof ParenthesisToken){
-                sb.append(((ParenthesisToken) token).getParenthesisType().getSymbol());
-            }else if(token instanceof OperatorToken){
-                sb.append(((OperatorToken) token).getOperatorType().getSymbol());
-            }
-        }
-
-        System.out.println("토큰"+sb);
     }
 
     public int getPostfixSize() {
@@ -62,11 +49,8 @@ public class CalculatorService {
         }else if (token instanceof OperatorToken) {
             double num2 = ((NumberToken) resultStack.pop()).getNumber();
             double num1 = ((NumberToken) resultStack.pop()).getNumber();
-
-
             double result = ((OperatorToken) token).calculate(num1, num2);
             resultStack.push(new Number(result));
-
             solving.updateNumberOfSolving();
 
             return true;
@@ -90,11 +74,8 @@ public class CalculatorService {
         return result;
     }
 
-    private Double getOrDefault(Double defaultValue) {
-        if (resultStack.isEmpty()) {
-            return defaultValue;
-        }
-        return ((NumberToken) resultStack.pop()).getNumber();
+    public String getProblem(){
+        return problem.getProblemText();
     }
 
 
