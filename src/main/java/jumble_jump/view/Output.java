c8521e7ded84f8dc1209fdb998ba00caa603de01
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-26 01:08:58 +0900

    feat : 출력 메서드 작성

--- src/main/java/jumble_jump/view/Output.java
+++ src/main/java/jumble_jump/view/Output.java
@@ -10,5 +10,42 @@ public class Output {
     public static void printServiceMessage(String message) {
         System.out.print(message);
     }
+
+    public static void printIntermediateStepAndCount(int numberOfSolving,String intermediateStep){
+        printNumberOfSolving(numberOfSolving);
+        printIntermediateStep(intermediateStep);
+        System.out.println();
+    }
+
+    private static void printNumberOfSolving(int numberOfSolving){
+        System.out.printf("풀이 횟수 : %d회\n", numberOfSolving);
+    }
+
+    private static void printIntermediateStep(String intermediateStep) {
+        System.out.printf("풀이과정 : %s\n", intermediateStep);
+    }
+
+    public static void printResult(int numberOfSolving,String Problem,Number result){
+        System.out.println("\n-------결과를 출력합니다.-------\n");
+        printProblem(Problem);
+        printTotalNumberOfSolving(numberOfSolving);
+        System.out.println();
+        printTotalNumberOfSolving(result);
+    }
+
+    private static void printProblem(String Problem) {
+        System.out.printf("문제 : %s\n", Problem);
+    }
+
+    private static void printTotalNumberOfSolving(int numberOfSolving){
+        System.out.printf("총 풀이 횟수 : %d회\n", numberOfSolving);
+    }
+
+    private static void printTotalNumberOfSolving(Number result){
+        System.out.printf("답 : %s\n", result);
+    }
+
+
+
 }
 
