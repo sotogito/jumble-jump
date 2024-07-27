429742d749670f9667a53c0253608c03b977990d
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-27 20:13:03 +0900

    test : 테스트 코드 작성

--- src/test/java/jumble_jump/ApplicationTest.java
+++ src/test/java/jumble_jump/ApplicationTest.java
@@ -15,13 +15,15 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class ApplicationTest {
 
     @Test
-    void application() {
-
+    void application1() {
         NumberMatcher numberMatcher = new NumberMatcher();
         OperatorMatcher operatorMatcher = new OperatorMatcher();
         ParenthesisMatcher parenthesisMatcher = new ParenthesisMatcher();
@@ -44,6 +46,174 @@ class ApplicationTest {
         assertEquals(expected, calculatorService.getResult());
     }
 
+    @Test
+    void application2() {
+        NumberMatcher numberMatcher = new NumberMatcher();
+        OperatorMatcher operatorMatcher = new OperatorMatcher();
+        ParenthesisMatcher parenthesisMatcher = new ParenthesisMatcher();
+        PostfixExpressionManager postfixExpressionManager = new PostfixExpressionManager();
+        ProblemToPostFixConverter infixPostFixHelper = new ProblemToPostFixConverter(postfixExpressionManager);
+
+        String inputProblem = "-100+99";
+        double expected = -1;
+
+        Tokenizer tokenizer = new Tokenizer(numberMatcher, operatorMatcher, parenthesisMatcher);
+        List<Token> tokens = tokenizer.tokenize(inputProblem);
+        Problem problem = new Problem(tokens);
+        Solving solving = new Solving();
+
+        CalculatorService calculatorService = new CalculatorService(problem,solving,infixPostFixHelper);
+
+        CalculateController calculateController =  new CalculateController(calculatorService);
+        calculateController.calculate();
+
+        assertEquals(expected, calculatorService.getResult());
+    }
+
+    @Test
+    void application3() {
+        NumberMatcher numberMatcher = new NumberMatcher();
+        OperatorMatcher operatorMatcher = new OperatorMatcher();
+        ParenthesisMatcher parenthesisMatcher = new ParenthesisMatcher();
+        PostfixExpressionManager postfixExpressionManager = new PostfixExpressionManager();
+        ProblemToPostFixConverter infixPostFixHelper = new ProblemToPostFixConverter(postfixExpressionManager);
+
+        String inputProblem = "-(100-99)";
+        double expected = -1;
+
+        Tokenizer tokenizer = new Tokenizer(numberMatcher, operatorMatcher, parenthesisMatcher);
+        List<Token> tokens = tokenizer.tokenize(inputProblem);
+        Problem problem = new Problem(tokens);
+        Solving solving = new Solving();
+
+        CalculatorService calculatorService = new CalculatorService(problem,solving,infixPostFixHelper);
+
+        CalculateController calculateController =  new CalculateController(calculatorService);
+        calculateController.calculate();
+
+        assertEquals(expected, calculatorService.getResult());
+    }
+
+    @Test
+    void application4() {
+        NumberMatcher numberMatcher = new NumberMatcher();
+        OperatorMatcher operatorMatcher = new OperatorMatcher();
+        ParenthesisMatcher parenthesisMatcher = new ParenthesisMatcher();
+        PostfixExpressionManager postfixExpressionManager = new PostfixExpressionManager();
+        ProblemToPostFixConverter infixPostFixHelper = new ProblemToPostFixConverter(postfixExpressionManager);
+
+        String inputProblem = "(100-99) - -1";
+        double expected = 2;
+
+        Tokenizer tokenizer = new Tokenizer(numberMatcher, operatorMatcher, parenthesisMatcher);
+        List<Token> tokens = tokenizer.tokenize(inputProblem);
+        Problem problem = new Problem(tokens);
+        Solving solving = new Solving();
+
+        CalculatorService calculatorService = new CalculatorService(problem,solving,infixPostFixHelper);
+
+        CalculateController calculateController =  new CalculateController(calculatorService);
+        calculateController.calculate();
+
+        assertEquals(expected, calculatorService.getResult());
+    }
+
+
+    @Test
+    void application5() {
+        NumberMatcher numberMatcher = new NumberMatcher();
+        OperatorMatcher operatorMatcher = new OperatorMatcher();
+        ParenthesisMatcher parenthesisMatcher = new ParenthesisMatcher();
+        PostfixExpressionManager postfixExpressionManager = new PostfixExpressionManager();
+        ProblemToPostFixConverter infixPostFixHelper = new ProblemToPostFixConverter(postfixExpressionManager);
+
+        String inputProblem = "7(1+1)";
+        double expected = 14;
+
+        Tokenizer tokenizer = new Tokenizer(numberMatcher, operatorMatcher, parenthesisMatcher);
+        List<Token> tokens = tokenizer.tokenize(inputProblem);
+        Problem problem = new Problem(tokens);
+        Solving solving = new Solving();
+
+        CalculatorService calculatorService = new CalculatorService(problem,solving,infixPostFixHelper);
+
+        CalculateController calculateController =  new CalculateController(calculatorService);
+        calculateController.calculate();
+
+        assertEquals(expected, calculatorService.getResult());
+    }
+
+    @Test
+    void application6() {
+        NumberMatcher numberMatcher = new NumberMatcher();
+        OperatorMatcher operatorMatcher = new OperatorMatcher();
+        ParenthesisMatcher parenthesisMatcher = new ParenthesisMatcher();
+        PostfixExpressionManager postfixExpressionManager = new PostfixExpressionManager();
+        ProblemToPostFixConverter infixPostFixHelper = new ProblemToPostFixConverter(postfixExpressionManager);
+
+        String inputProblem = "-7(1+1)";
+        double expected = -14;
+
+        Tokenizer tokenizer = new Tokenizer(numberMatcher, operatorMatcher, parenthesisMatcher);
+        List<Token> tokens = tokenizer.tokenize(inputProblem);
+        Problem problem = new Problem(tokens);
+        Solving solving = new Solving();
+
+        CalculatorService calculatorService = new CalculatorService(problem,solving,infixPostFixHelper);
+
+        CalculateController calculateController =  new CalculateController(calculatorService);
+        calculateController.calculate();
+
+        assertEquals(expected, calculatorService.getResult());
+    }
+
+    @Test
+    void application7() {
+        NumberMatcher numberMatcher = new NumberMatcher();
+        OperatorMatcher operatorMatcher = new OperatorMatcher();
+        ParenthesisMatcher parenthesisMatcher = new ParenthesisMatcher();
+        PostfixExpressionManager postfixExpressionManager = new PostfixExpressionManager();
+        ProblemToPostFixConverter infixPostFixHelper = new ProblemToPostFixConverter(postfixExpressionManager);
+
+        String inputProblem = "-{2(1+1)}";
+        double expected = -4;
+
+        Tokenizer tokenizer = new Tokenizer(numberMatcher, operatorMatcher, parenthesisMatcher);
+        List<Token> tokens = tokenizer.tokenize(inputProblem);
+        Problem problem = new Problem(tokens);
+        Solving solving = new Solving();
+
+        CalculatorService calculatorService = new CalculatorService(problem,solving,infixPostFixHelper);
+
+        CalculateController calculateController =  new CalculateController(calculatorService);
+        calculateController.calculate();
+
+        assertEquals(expected, calculatorService.getResult());
+    }
+
+    @Test
+    void application8() {
+        NumberMatcher numberMatcher = new NumberMatcher();
+        OperatorMatcher operatorMatcher = new OperatorMatcher();
+        ParenthesisMatcher parenthesisMatcher = new ParenthesisMatcher();
+        PostfixExpressionManager postfixExpressionManager = new PostfixExpressionManager();
+        ProblemToPostFixConverter infixPostFixHelper = new ProblemToPostFixConverter(postfixExpressionManager);
+
+        String inputProblem = "[{3 * (4 + 2)} - 5] / 2";
+        double expected = 6.5;
+
+        Tokenizer tokenizer = new Tokenizer(numberMatcher, operatorMatcher, parenthesisMatcher);
+        List<Token> tokens = tokenizer.tokenize(inputProblem);
+        Problem problem = new Problem(tokens);
+        Solving solving = new Solving();
+
+        CalculatorService calculatorService = new CalculatorService(problem,solving,infixPostFixHelper);
+
+        CalculateController calculateController =  new CalculateController(calculatorService);
+        calculateController.calculate();
+
+        assertEquals(expected, calculatorService.getResult());
+    }
 
 
 }
