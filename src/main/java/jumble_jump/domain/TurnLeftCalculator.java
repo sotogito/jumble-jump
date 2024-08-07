942f1abfa488cecdda13742f75502e15837856a5
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-08 03:23:53 +0900

    feat : 왼쪽, 뒤로 이동좌표 계산 클래스 작성

---
+++ src/main/java/jumble_jump/domain/TurnLeftCalculator.java
@@ -0,0 +1,38 @@
+package jumble_jump.domain;
+
+import jumble_jump.domain.type.Direction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TurnLeftCalculator {
+    private final List<Integer> dx = new ArrayList<>(
+            List.of(-1, 0, 1, 0));
+
+    private final List<Integer> dy = new ArrayList<>(
+            List.of(0, 1, 0, -1));
+
+    public TurnLeftCalculator() {
+    }
+
+    public Direction getNextDirection(Direction nowDirection){
+        return Direction.getLeftTurnDirection(nowDirection);
+    }
+
+    public int getNextX(int x, int directionIndex){
+        return x + dx.get(directionIndex);
+    }
+
+    public int getNextY(int y,int directionIndex){
+        return y + dy.get(directionIndex);
+    }
+
+    public int getBackX(int x, int directionIndex){
+        return x - dx.get(directionIndex);
+    }
+
+    public int getBackY(int y,int directionIndex){
+        return y - dy.get(directionIndex);
+    }
+
+}
