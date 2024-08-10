67907b3d9ebd281f81b5465a178a4e406cce91fe
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-10 19:03:11 +0900

    feat : open NLP 라이브러리 추가

---
+++ src/main/java/jumble_jump/controller/example.java
@@ -0,0 +1,30 @@
+package jumble_jump.controller;
+
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class example {
+    public static void main(String[] args) {
+        try (InputStream modelIn = example.class.getResourceAsStream("/opennlp-en-ud-ewt-sentence-1.0-1.9.3.bin")) {
+            if (modelIn == null) {
+                throw new IllegalArgumentException("파일을 찾을 수 없습니다.");
+            }
+
+            SentenceModel model = new SentenceModel(modelIn);
+            SentenceDetectorME sentenceDetector = new SentenceDetectorME(model);
+
+            String text = "This is a test. Let's see if it works.";
+            String[] sentences = sentenceDetector.sentDetect(text);
+
+            for (String sentence : sentences) {
+                System.out.println(sentence);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
