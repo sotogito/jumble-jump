b83f8d64950d069e6d42ca7e5449e42f028e00a3
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-13 00:36:13 +0900

    feat : 번역 한국어 문장 입력 input 작성

---
+++ src/main/java/jumble_jump/view/Input.java
@@ -0,0 +1,12 @@
+package jumble_jump.view;
+
+import java.util.Scanner;
+
+public class Input {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String inputKorean(){
+        return scanner.nextLine();
+    }
+}
+
