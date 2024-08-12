2d6db8a661e87989eba131dee9f2022dc781b607
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-13 00:33:11 +0900

    feat : 한국어 번역전 원본 저장 클래스 생성

---
+++ src/main/java/jumble_jump/domain/Korean.java
@@ -0,0 +1,12 @@
+package jumble_jump.domain;
+
+public class Korean {
+    private String koran;
+
+    public Korean() {
+    }
+
+    public void setKoran(String koran) {
+        this.koran = koran;
+    }
+}
