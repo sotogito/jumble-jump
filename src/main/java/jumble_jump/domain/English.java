342dcee9a1fc13a8e0aa56b1e50c058495d19c8b
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-13 00:33:32 +0900

    feat : 번역후 문장 저장 English 클래스 생성

---
+++ src/main/java/jumble_jump/domain/English.java
@@ -0,0 +1,12 @@
+package jumble_jump.domain;
+
+public class English {
+    private String english;
+
+    public English() {
+    }
+
+    public void setEnglish(String english) {
+        this.english = english;
+    }
+}
