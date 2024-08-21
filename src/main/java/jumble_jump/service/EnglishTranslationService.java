8c976dc0ca7e5baa4fdec68f992630f1e3748642
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-21 16:56:00 +0900

    feat : 한국어 영어로 번역 서비스 구현

--- src/main/java/jumble_jump/service/EnglishTranslationService.java
+++ src/main/java/jumble_jump/service/EnglishTranslationService.java
@@ -10,23 +10,22 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 public class EnglishTranslationService {
-    private final TranslationEntryRepository translationEntryRepository;
-
     private final String API_KEY = "1f3905db-c5c1-4cc1-8681-1229b5f5c555:fx"; // DeepL API Key
     private final String END_POINT = "https://api-free.deepl.com/v2/translate";
     private final String translateLanguage = "EN";
+
+    private final TranslationEntryRepository translationEntryRepository;
 
     public EnglishTranslationService(TranslationEntryRepository translationEntryRepository) {
         this.translationEntryRepository = translationEntryRepository;
     }
 
     public void translate(String korean) throws Exception {
-        setKorean(korean);
-        String english = translateToEnglish(korean);
-        translationEntryRepository.setEnglish(english);
+        translationEntryRepository.setKorean(korean);
+        translationEntryRepository.setEnglish(translateToEnglish(korean));
     }
 
-    public String translateToEnglish(String korean) throws Exception {
+    private String translateToEnglish(String korean) throws Exception {
         OkHttpClient client = new OkHttpClient();
 
         MediaType mediaType = MediaType.get("application/x-www-form-urlencoded");
@@ -49,8 +48,5 @@ public class EnglishTranslationService {
         }
     }
 
-    public void setKorean(String korean){
-        translationEntryRepository.setKorean(korean);
-    }
 
 }
