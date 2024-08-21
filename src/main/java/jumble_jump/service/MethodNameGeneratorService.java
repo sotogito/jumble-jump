922c9dbdc6a96a64c3876a68b9b36d2f373ba717
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-21 16:58:29 +0900

    feat : 메서드 이름 생성 상위 서비스 구현

--- src/main/java/jumble_jump/service/MethodNameGeneratorService.java
+++ src/main/java/jumble_jump/service/MethodNameGeneratorService.java
@@ -1,4 +1,25 @@
 package jumble_jump.service;
 
+import jumble_jump.repository.TranslationEntryRepository;
+
 public class MethodNameGeneratorService {
+    private final EnglishTranslationService translationService;
+    private final NLPProcessingService nlpService;
+    private final CamelFormattingService camelFormattingService;
+
+    public MethodNameGeneratorService(
+            EnglishTranslationService translationService, NLPProcessingService nlpService, CamelFormattingService camelFormattingService) {
+        this.camelFormattingService = camelFormattingService;
+        this.translationService = translationService;
+        this.nlpService = nlpService;
+
+    }
+
+    public void generate(String inputKorean) throws Exception {
+        translationService.translate(inputKorean);
+        nlpService.handlePos();
+        camelFormattingService.formatToCamelCase();
+    }
+
+
 }
