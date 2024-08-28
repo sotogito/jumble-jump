30715f593fa54a43949b0c27f59fd5677db9e17d
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-28 18:02:43 +0900

    feat : 박수 출력물 삭제

--- src/main/java/jumble_jump/domain/ClapImpl.java
+++ src/main/java/jumble_jump/domain/ClapImpl.java
@@ -3,7 +3,6 @@ package jumble_jump.domain;
 import jumble_jump.domain.type.GameElement;
 
 public class ClapImpl implements Clap{
-    private final String clap = "짝";
     private final int count;
 
     public ClapImpl(int count) {
