9082d4a7cc40dbdc7b46b11a088813d6b96383b3
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-28 18:15:22 +0900

    feat : 숫자 입력받기 inputview 작성

---
+++ src/main/java/jumble_jump/view/Input.java
@@ -0,0 +1,13 @@
+package jumble_jump.view;
+
+
+import java.util.Scanner;
+
+public class Input {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String inputNumbers(){
+        return scanner.nextLine();
+    }
+}
