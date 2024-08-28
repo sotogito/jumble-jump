39920a0737010ee50e03c164526521c379985813
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-28 16:46:42 +0900

    feat : 박수 클래스 작성

--- src/main/java/jumble_jump/domain/Clap.java
+++ src/main/java/jumble_jump/domain/Clap.java
@@ -1,4 +1,15 @@
 package jumble_jump.domain;
 
 public class Clap implements Token{
+    private final String clap = "짝";
+    private final int count;
+
+    public Clap(int count) {
+        this.count = count;
+    }
+
+    public int getClap() {
+        return count;
+    }
+
 }
