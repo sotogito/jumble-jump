660ee6773ee14226ae8e8d3981d255d15d0d5a15
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-28 16:50:18 +0900

    feat : 숫자+박수 레퍼지토리 작성

--- src/main/java/jumble_jump/repository/Tokens369.java
+++ src/main/java/jumble_jump/repository/Tokens369.java
@@ -1,4 +1,14 @@
 package jumble_jump.repository;
 
+import jumble_jump.domain.Token;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tokens369 {
+    private final List<Token> tokens;
+
+    public Tokens369(List<Token> tokens) {
+        this.tokens = tokens;
+    }
 }
