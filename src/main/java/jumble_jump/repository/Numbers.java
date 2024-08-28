a3b572ced7424f5f75f6fb4e203325adb2820573
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-28 16:57:21 +0900

    feat : 숫자 레퍼지토리 숫자 인터페이스로 자료형 변경

--- src/main/java/jumble_jump/repository/Numbers.java
+++ src/main/java/jumble_jump/repository/Numbers.java
@@ -1,18 +1,19 @@
 package jumble_jump.repository;
 
+import jumble_jump.domain.NumberT;
 import jumble_jump.domain.Token;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Numbers {
-    private final List<Token> numbers;
+    private final List<NumberT> numbers;
 
-    public Numbers(List<Token> numbers) {
+    public Numbers(List<NumberT> numbers) {
         this.numbers = numbers;
     }
 
-    public List<Token> getNumbers() {
+    public List<NumberT> getNumbers() {
         return numbers;
     }
 
