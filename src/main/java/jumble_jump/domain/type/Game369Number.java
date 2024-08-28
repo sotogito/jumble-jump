df6c72fcbc1ed17d864c7c9090754e9a6fafa779
sotogito <108535526+sotogito@users.noreply.github.com>
2024-08-28 17:28:55 +0900

    feat : 3,6,9 숫자 Enum 으로 생성

---
+++ src/main/java/jumble_jump/domain/type/Game369Number.java
@@ -0,0 +1,30 @@
+package jumble_jump.domain.type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum Game369Number {
+    THREE(3),
+    SIX(6),
+    NINE(9)
+    ;
+
+    private int number;
+
+    Game369Number(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public static List<Integer> get369NumberList(){
+        List<Integer> result = new ArrayList<>();
+
+        for (Game369Number game369Number : Game369Number.values()) {
+            result.add(game369Number.getNumber());
+        }
+        return result;
+    }
+}
