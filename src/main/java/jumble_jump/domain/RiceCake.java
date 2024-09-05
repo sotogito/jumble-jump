d42d5a3956d664c84b2a8a689f3e1c409573c597
sotogito <108535526+sotogito@users.noreply.github.com>
2024-09-05 18:27:02 +0900

    feat : 떡 객체 작성

---
+++ src/main/java/jumble_jump/domain/RiceCake.java
@@ -0,0 +1,22 @@
+package jumble_jump.domain;
+
+public class RiceCake {
+    private final int height;
+
+    public RiceCake(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getResultOfCuttingWithCutter(int cutterHeight){
+        return height - cutterHeight;
+    }
+
+    public boolean canCut(int cutterHeight) {
+        return height > cutterHeight;
+    }
+
+}
