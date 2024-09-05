47e9f38260eaff99907818c2a538f73b101defc5
sotogito <108535526+sotogito@users.noreply.github.com>
2024-09-05 21:10:32 +0900

    fix : 재귀 조건 변경

--- src/main/java/jumble_jump/serive/CutterHeightSettingService.java
+++ src/main/java/jumble_jump/serive/CutterHeightSettingService.java
@@ -31,36 +31,77 @@ public class CutterHeightSettingService {
          * 재귀
          */
 
+        List<Integer> array = order.getArray();
         List<RiceCake> riceCakeList = order.getSortedRiceCakes();
         int targetLength = order.getRiceCakeLength();
         int start = 0;
-        int end = order.getRiceCakeCount()-1;
+        int end = array.size()-1;
 
-        int heightResult = findCutterHeight(riceCakeList,targetLength,start,end);
+        int heightResult = findCutterHeight(riceCakeList,array,targetLength,start,end);
 
+        if(heightResult == -1){
+            throw new IllegalArgumentException("계산할 수 없습니다.");
+        }
         cutter.setHeight(heightResult);
     }
 
-    private int findCutterHeight(List<RiceCake> array, int target, int start, int end){
+    private int findCutterHeight(
+            List<RiceCake> riceCakeList, List<Integer> array, int target, int start, int end){
+        if (start > end) {
+            return -1;  // 정상적인 기저 조건: 더 이상 탐색할 수 없을 때
+        }
+
+
+
+
         int mid = (start + end) / 2;
-        int midHeight = array.get(mid).getHeight();
+        int midHeight = array.get(mid);
+        int cutRiceCakeHeight = calculateCutRiceCake(riceCakeList,midHeight);
+
+        System.out.println(mid);
+        System.out.println(midHeight);
+        System.out.println(cutRiceCakeHeight);
+
+        CutterLength midCutterLength = cutterController.calculate(target, cutRiceCakeHeight);
 
 
-        CutterLength midCutterLength = cutterController.calculate(target, midHeight);
 
         if (midCutterLength.equals(CutterLength.SAME)){
             return midHeight;
 
         } else if (midCutterLength.equals(CutterLength.LONG)){
-            int endPoint = mid + (midCutterLength.getValue());
-            return findCutterHeight(array, target, start, endPoint);
+            int startPoint = mid + (midCutterLength.getValue());
+            return findCutterHeight(riceCakeList,array, target,startPoint, end); //짧게
 
         }else if (midCutterLength.equals(CutterLength.SHORT)){
-            int startPoint = mid + (midCutterLength.getValue());
-            return findCutterHeight(array, target, startPoint, end);
+            int endPoint = mid + (midCutterLength.getValue());
+            return findCutterHeight(riceCakeList, array, target, start, endPoint);
         }
 
-        throw new IllegalArgumentException("계산할 수 없습니다.");
+        return -1;
+    }
+
+    private int calculateCutRiceCake(List<RiceCake> riceCakeList, int midCutterHeight){
+        int result = 0;
+
+        for (RiceCake riceCake : riceCakeList) {
+            int riceCakeHeight = riceCake.getHeight();
+
+            if(riceCakeHeight > midCutterHeight){
+                result += (riceCakeHeight - midCutterHeight);
+            }
+        }
+        return result;
+
+    }
+
+
+
+
+    //note 출력
+
+    public int getResultCutterHeight(){
+        return cutter.getHeight();
     }
 
 }
