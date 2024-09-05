97e015e0111d01ca51818805b895bebe5cb69818
sotogito <108535526+sotogito@users.noreply.github.com>
2024-09-05 21:09:00 +0900

    feat : 컨트롤러 작성

--- src/main/java/jumble_jump/controller/MainController.java
+++ src/main/java/jumble_jump/controller/MainController.java
@@ -1,8 +1,46 @@
 package jumble_jump.controller;
 
+import jumble_jump.domain.Cutter;
+import jumble_jump.domain.CutterControlUnit;
+import jumble_jump.domain.RiceCake;
+import jumble_jump.repository.Order;
+import jumble_jump.serive.CutterHeightSettingService;
+import jumble_jump.util.InputRiceCakeParser;
+import jumble_jump.util.OrderFactory;
+import jumble_jump.view.Input;
+
+import java.util.List;
+
 public class MainController {
+    private CutterHeightSettingService cutterHeightSettingService;
 
     public void main(){
+        Order order = createOrder();
+        createCutterHeightSettingService(order);
+
+        cutterHeightSettingService.setCutterHeight();
+
+        System.out.println(cutterHeightSettingService.getResultCutterHeight());
 
     }
+
+    private void createCutterHeightSettingService(Order order){
+        cutterHeightSettingService = new CutterHeightSettingService(
+                new Cutter(),order, new CutterControlUnit());
+    }
+
+
+    private Order createOrder(){
+        while (true){
+            try{
+                List<RiceCake> riceCakeeList = InputRiceCakeParser.parse(Input.inputRiceCakes());
+                int totalHeight = Input.inputRiceCakeTotalHeight();
+
+                return OrderFactory.createOrder(riceCakeeList, totalHeight);
+            }catch (NumberFormatException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
 }
